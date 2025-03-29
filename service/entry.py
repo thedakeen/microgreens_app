a496c3d3d9a199d3267eba2ec832c4315d0e43c8
Ansar <ansaramanzholov2005@gmail.com>
2025-03-29 18:05:12 +0500

    added microgreens|entry

--- service/entry.py
+++ service/entry.py
@@ -2,6 +2,7 @@ from sqlalchemy import select, exc
 from data.config import new_session, EntryOrm
 from models.entry import EntryCreate, EntryRead
 from fastapi import HTTPException, status
+from typing import List
 
 
 async def create_entry(data: EntryCreate, lot_id: int) -> EntryRead | None:
@@ -17,3 +18,17 @@ async def create_entry(data: EntryCreate, lot_id: int) -> EntryRead | None:
     except AttributeError:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Some parameters do not exist")
 
+
+async def get_entries(lot_id: int) -> List[EntryRead] | None:
+    try:
+        async with new_session() as session:
+            result = await session.execute(select(EntryOrm).filter(EntryOrm.lot_id == lot_id))
+            entries = result.scalars().all()
+
+            if not entries:
+                return None
+
+            return [EntryRead.from_orm(entry) for entry in entries]
+    except Exception as e:
+        print(f"Ошибка при получении записей: {e}")
+        return None
