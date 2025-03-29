cca55b899ab94725a8b8e244267aec8e150b1c6c
thedakeen <thedakeen@gmail.com>
2025-03-30 00:56:30 +0500

    cors

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from dotenv import load_dotenv
 from contextlib import asynccontextmanager
@@ -27,12 +28,20 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 static_dir = os.path.join(base_dir, "static")
 
 app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
 app.add_middleware(AuthMiddleware)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["localhost:8081"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(user.router)
 app.include_router(lot.router)
 app.include_router(entry.router)
